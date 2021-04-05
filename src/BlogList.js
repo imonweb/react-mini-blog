b72107abdeeb8bb58de34be0570151ffd30942a8
Imon <contact@imonweb.co.uk>
2021-04-05 20:21:51 +0100

    json-server added

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,6 @@
 
 // const BlogList = (props) => {
-const BlogList = ({blogs, title, handleDelete}) => {
+const BlogList = ({blogs, title}) => {
   // const blogs = props.blogs;
   // const title = props.title;
 
@@ -12,7 +12,7 @@ const BlogList = ({blogs, title, handleDelete}) => {
        <div className="blog-preview" key={blog.id}>
          <h2>{ blog.title }</h2>
          <p>Written by: { blog.author }</p>
-         <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+         {/* <button onClick={() => handleDelete(blog.id)}>delete blog</button> */}
        </div>
      ))}
      </div>
--- src/Home.js
+++ src/Home.js
@@ -2,25 +2,37 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+ 
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
 
   useEffect(() => {
-    console.log('use effect ran')
-  });
+    setTimeout( () => {
+      fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        // console.log(data);
+        setBlogs(data)
+        setIsPending(false);
+      })
+      .catch(err => {
+        console.log(err.message);
+      }) 
+    })
+  }, []);
   
   return ( 
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>  
-      <BlogList blogs={blogs.filter( (blog) => blog.author === 'mario')} title="Mario's Blogs" />  
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} title="All Blogs!"/>  }
+      
+      
     </div>
    );
 }
